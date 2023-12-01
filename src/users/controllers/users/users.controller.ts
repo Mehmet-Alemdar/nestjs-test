5b0f5fc05cad195f3386a9cb8d680cef6a6586f0
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-12-01 15:30:18 +0300

    create get and post for user

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { CreateUserDto } from '../../dtos/CreateUser.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return [{ id: 1, username: 'john' }];
+  }
+
+  @Post('create')
+  createUser(@Body() userData: CreateUserDto) {
+    console.log(userData)
+    return {};
+  }
+}
